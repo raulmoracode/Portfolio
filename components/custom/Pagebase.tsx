324cf8be73d14b7285c1415b253b89a8f616febc
Raulmora22 <raulprogrammerr@gmail.com>
2026-05-19 14:54:02 +0200

    New pagebase version adaptive

--- components/custom/Pagebase.tsx
+++ components/custom/Pagebase.tsx
@@ -15,9 +15,9 @@ export default function PageBase({
   const isRootPath = pathname === "/" || pathname === "/links";
   const router = useRouter();
   return (
-    <div className="flex min-h-screen flex-col items-center font-sans pt-2 sm:pt-10">
+    <div className="flex min-h-screen flex-col items-center font-sans">
       {showThemeToggle && <ModeToggle />}
-      <main className="flex w-full max-w-4xl flex-col py-12 sm:py-24 px-8 sm:flex-1">
+      <main className="flex w-full max-w-4xl flex- md:pt-25 sm:pt-40 sm:pb-1 pt-20 px-8 flex-1">
         <section className="space-y-4 mb-6 sm:mb-10">
           <h1 className="text-4xl font-bold tracking-tighter">{title}</h1>
           <p className="max-w-150 leading-snug text-zinc-500 dark:text-zinc-400 text-lg ">
